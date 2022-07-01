08944a85727cb977770077aada4621ba63e4d11c
Dastan Alymbek uulu <alymbek.uulu.d@gmail.com>
2022-07-01 17:30:05 +0600

    config Nginx

--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('articles.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
